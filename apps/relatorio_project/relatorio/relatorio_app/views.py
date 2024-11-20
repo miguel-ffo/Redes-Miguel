ea7a4c9b6fdd05f5282c7fb9548dec3274969d5a
Miguel Figueiredo <miguelgamesjua@gmail.com>
2024-11-20 17:05:37 -0300

    erro json

--- apps/relatorio_project/relatorio/relatorio_app/views.py
+++ apps/relatorio_project/relatorio/relatorio_app/views.py
@@ -6,9 +6,8 @@ import requests
 def index_relatorios(request):
     return render(request, 'index_relatorios.html')
 
-
 def clientes_ativos(request):
-    url = "http://cliente-api:8000/clientes/"
+    url = "http://cliente-api:8000/api/clientes/"  # Alteração: usaremos o nome do serviço cliente-api
     try:
         response = requests.get(url)
         response.raise_for_status()  # Lança um erro se o código de status não for 2xx
@@ -26,9 +25,8 @@ def clientes_ativos(request):
     
     return render(request, 'clientes_ativos.html', {'clientes_ativos': ativos})
 
-
 def novos_registros(request):
-    url = "http://cliente-api:8000/clientes/"
+    url = "http://cliente-api:8000/api/clientes/"  # Alteração
     response = requests.get(url).json()
     novos = []
 
@@ -43,24 +41,26 @@ def novos_registros(request):
 
     return render(request, 'novos_registros.html', {'novos_registros': novos})
 
+
 def produtos_populares(request):
-    url = "http://127.0.0.1:8001/inventario/produtos/"
+    url = "http://inventario-api:8001/inventario/produtos/"  # Alteração: agora usamos inventario-api
     response = requests.get(url).json()
     populares = sorted(response, key=lambda x: x['alteracoes'], reverse=True)[:5]
 
     return render(request, 'produtos_populares.html', {'produtos_populares': populares})
 
 def estoque_baixo(request):
-    url = "http://127.0.0.1:8001/inventario/produtos/"
+    url = "http://inventario-api:8001/inventario/produtos/"  # Alteração
     response = requests.get(url).json()
     limite_critico = int(request.GET.get('limite', 10))
     baixos = [produto for produto in response if produto['quantidade'] < limite_critico]
 
     return render(request, 'estoque_baixo.html', {'estoque_baixo': baixos})
 
+
 def insights_gerais(request):
-    clientes_url = "http://127.0.0.1:8000/clientes/"
-    inventario_url = "http://127.0.0.1:8001/inventario/produtos/"
+    clientes_url = "http://cliente-api:8000/api/clientes/"  # Alteração
+    inventario_url = "http://inventario-api:8001/inventario/produtos/"  # Alteração
 
     clientes = requests.get(clientes_url).json()
     produtos = requests.get(inventario_url).json()
@@ -80,3 +80,4 @@ def insights_gerais(request):
     }
 
     return render(request, 'insights_gerais.html', context)
+
