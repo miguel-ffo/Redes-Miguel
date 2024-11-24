6aca89c43bb8a609b6e0daa46e5e9784b9e5271c
Miguel Figueiredo <miguelgamesjua@gmail.com>
2024-11-24 15:56:56 -0300

    css

--- apps/relatorio_project/relatorio/relatorio_app/urls.py
+++ apps/relatorio_project/relatorio/relatorio_app/urls.py
@@ -4,18 +4,18 @@ from . import views
 urlpatterns = [
     
     #index
-    path('', views.home, name='home'),
+    path('principal/', views.home, name='home'),
     
-    path('relatorios/', views.index_relatorios, name='index_relatorios'),
+    path('', views.index_relatorios, name='index_relatorios'),
     #Relatorios de clientes
     
     path('clientes/ativos/', views.clientes_ativos, name='clientes_ativos'),
-    path('clientes/novos/', views.novos_registros, name='novos_registros'),
+    path('relatorios/clientes/novos/', views.novos_registros, name='novos_registros'),
     
     #Relatorios de inventarios
-    path('inventario/populares/', views.produtos_populares, name='produtos_populares'),
-    path('inventario/estoque-baixo/', views.estoque_baixo, name='estoque_baixo'),
+    path('relatorios/inventario/populares/', views.produtos_populares, name='produtos_populares'),
+    path('relatorios/inventario/estoque-baixo/', views.estoque_baixo, name='estoque_baixo'),
     
     #Insights gerais
-    path('insights/gerais/', views.insights_gerais, name='insights_gerais'),
+    path('relatorios/insights/gerais/', views.insights_gerais, name='insights_gerais'),
 ]
